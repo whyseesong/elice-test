4a529005d479437d1e4380543e3a364f9b3e2ffd
SongYoungCheol <yc@sk.com>
2021-11-10 01:22:39 +0900

    css 정리

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,22 @@ import FileTree from "./components/FileTree";
 import { JsZipContext } from "./context/JsZipContext";
 import JSZip from "jszip";
 import TreeContainer from "./Containers/TreeContainer";
+import AppClass from "./App.module.css";
 
 function App() {
   return (
     <JsZipContext.Provider value={new JSZip()}>
       <TreeContainer>
         <div className="App">
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <div style={{ padding: "1rem" }}>
+          <div className={`${AppClass.Flex} ${AppClass.Center}`}>
+            <div className={AppClass.Padding}>
               <FileUploader />
             </div>
-            <div style={{ display: "flex" }}>
-              <div style={{ padding: "1rem" }}>
+            <div className={AppClass.Flex}>
+              <div className={AppClass.Padding}>
                 <FileTree />
               </div>
-              <div style={{ padding: "1rem" }}>
+              <div className={AppClass.Padding}>
                 <Editor />
               </div>
             </div>
@@ -38,11 +32,3 @@ function App() {
 }
 
 export default App;
-
-/*
- todo:
-    스타일드컴포넌트로 스타일 정리
-
-    zip 안에 Zip이 있는 경우 재귀적으로 jsZip인스턴스화
-    zip을 여러번 열면 머지되는 현상 수정
- */
